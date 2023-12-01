59b1f7ca134a9cb51fb602cc48c37a6ae155450e
Yevhen Yanchak <yanchakevgeniy@gmail.com>
2023-12-01 18:36:38 +0200

    add new game brain-gcd

---
+++ src/games/gcd.js
@@ -0,0 +1,26 @@
+import engin from '../index.js';
+import getRandomInt from '../utils.js';
+
+const getGcd = (a, b) => {
+  let firstNumber = Math.abs(a);
+  let secondNumber = Math.abs(b);
+  while (firstNumber !== secondNumber) {
+    if (firstNumber > secondNumber) firstNumber -= secondNumber;
+    else secondNumber -= firstNumber;
+  }
+  return firstNumber;
+};
+
+const getData = () => {
+  const firstNumber = getRandomInt(100);
+  const secondNumber = getRandomInt(100);
+  const question = `${firstNumber} ${secondNumber}`;
+  const answer = `${getGcd(firstNumber, secondNumber)}`;
+  return [question, answer];
+};
+const gcd = () => {
+  const rules = 'Find the greatest common divisor of given numbers.';
+  engin(rules, getData);
+};
+
+export default gcd;
